b45748329639b8c02f22612d465283d8df110ef8
Rahul <rvasant2@illinois.edu>
2024-05-08 16:58:02 -0500

    improved image loading and added reset button functionality. enjoy

--- static/js/ImageBox.js
+++ static/js/ImageBox.js
@@ -31,205 +31,10 @@ var imageBoxSettings = {
     height: 1024
 };
 
-window.wheelzoom = (function(){
-
-    var canvas = document.createElement('canvas');
-
-    var main = function(img, settings){
-        if (!img || !img.nodeName || img.nodeName !== 'IMG') { return; }
-
-        var width;
-        var height;
-        var previousEvent;
-        var cachedDataUrl;
-
-        function setSrcToBackground(img) {
-            img.style.backgroundImage = 'url("'+img.src+'")';
-            img.style.backgroundRepeat = 'no-repeat';
-            canvas.width = img.naturalWidth;
-            canvas.height = img.naturalHeight;
-            // canvas.height = Math.min(720, canvas.width * (img.imHeight / img.imWidth));
-            img.bgOffsetX = (canvas.width - img.naturalWidth)/2;
-            img.bgOffsetY = (canvas.height - img.naturalHeight)/2;
-            cachedDataUrl = canvas.toDataURL();
-            img.src = cachedDataUrl;
-
-            reset();
-        }
-
-        function updateBgStyle() {
-
-            var minX = -img.bgOffsetX;
-            var maxX = img.imWidth - img.bgWidth + img.bgOffsetX;
-            if (img.bgWidth - img.bgOffsetX * 2 >= img.imWidth) {
-                img.bgPosX = Math.max(Math.min(img.bgPosX, minX), maxX);
-            } else {
-                img.bgPosX = Math.min(Math.max(img.bgPosX, minX), maxX);
-            }
-
-            var minY = -img.bgOffsetY;
-            var maxY = img.imHeight - img.bgHeight + img.bgOffsetY;
-            if (img.bgHeight - img.bgOffsetY * 2 >= img.imHeight) {
-                img.bgPosY = Math.max(Math.min(img.bgPosY, minY), maxY);
-            } else {
-                img.bgPosY = Math.min(Math.max(img.bgPosY, minY), maxY);
-            }
-
-            img.style.backgroundSize = img.bgWidth+'px '+img.bgHeight+'px';
-            img.style.backgroundPosition = (img.bgOffsetX + img.bgPosX)+'px '+ (img.bgOffsetY + img.bgPosY)+'px';
-
-        }
-
-        function reset() {
-
-            if (canvas) {
-                var zoomFactor = 2*Math.min(canvas.width / img.imWidth, canvas.height / img.imHeight);
-            } else {
-                var zoomFactor = 2;
-            }
-
-            img.bgWidth = canvas.width;
-            img.bgHeight = canvas.height;
-            // var initialZoomLevel = 1;
-            // img.bgWidth = img.naturalWidth * initialZoomLevel; // initialZoomLevel could be 1 for full size
-            // img.bgHeight = img.naturalHeight * initialZoomLevel;
-            img.bgPosX = (img.imWidth - img.bgWidth) / 2;
-            img.bgPosY = (img.imHeight - img.bgHeight) / 2;
-            updateBgStyle();
-        }
-
-        function onwheel(e) {
-            var deltaY = 0;
-
-            e.preventDefault();
-
-            if (e.deltaY) { // FireFox 17+ (IE9+, Chrome 31+?)
-                deltaY = -e.deltaY;
-            } else if (e.wheelDelta) {
-                deltaY = e.wheelDelta;
-            }
-
-            // As far as I know, there is no good cross-browser way to get the cursor position relative to the event target.
-            // We have to calculate the target element's position relative to the document, and subtrack that from the
-            // cursor's position relative to the document.
-            var rect = img.getBoundingClientRect();
-            var offsetX = e.pageX - rect.left - window.pageXOffset - img.bgOffsetX;
-            var offsetY = e.pageY - rect.top - window.pageYOffset - img.bgOffsetY;
-
-            // Record the offset between the bg edge and cursor:
-            var bgCursorX = offsetX - img.bgPosX;
-            var bgCursorY = offsetY - img.bgPosY;
-
-            // Use the previous offset to get the percent offset between the bg edge and cursor:
-            var bgRatioX = bgCursorX/img.bgWidth;
-            var bgRatioY = bgCursorY/img.bgHeight;
-
-            var zoomFactor = 1 + settings.zoom;
-            if (deltaY >= 0) {
-                zoomFactor = 1 / zoomFactor;
-            }
-
-            img.bgWidth *= zoomFactor;
-            img.bgHeight *= zoomFactor;
-
-            // Take the percent offset and apply it to the new size:
-            img.bgPosX = offsetX - (img.bgWidth * bgRatioX);
-            img.bgPosY = offsetY - (img.bgHeight * bgRatioY);
-
-            updateBgStyle();
-        }
-
-        function drag(e) {
-            e.preventDefault();
-            img.bgPosX += (e.pageX - previousEvent.pageX);
-            img.bgPosY += (e.pageY - previousEvent.pageY);
-            previousEvent = e;
-            updateBgStyle();
-        }
-
-        function removeDrag() {
-            document.removeEventListener('mouseup', removeDrag);
-            document.removeEventListener('mousemove', drag);
-        }
-
-        // Make the background draggable
-        function draggable(e) {
-            e.preventDefault();
-            previousEvent = e;
-            document.addEventListener('mousemove', drag);
-            document.addEventListener('mouseup', removeDrag);
-        }
-
-        function load() {
-            if (img.src === cachedDataUrl) return;
-
-            img.imWidth = img.naturalWidth;
-            img.imHeight = img.naturalHeight;
-
-            img.bgWidth = img.imWidth;
-            img.bgHeight = img.imHeight;
-            img.bgPosX = 0;
-            img.bgPosY = 0;
-
-            img.style.backgroundSize     = img.bgWidth+'px '+img.bgHeight+'px';
-            img.style.backgroundPosition = img.bgPosX+' '+img.bgPosY;
-
-            setSrcToBackground(img);
-
-            img.addEventListener('wheelzoom.reset', reset);
-            img.addEventListener('wheel', onwheel);
-            img.addEventListener('mousedown', draggable);
-        }
-
-        var destroy = function (originalProperties) {
-            img.removeEventListener('wheelzoom.destroy', destroy);
-            img.removeEventListener('wheelzoom.reset', reset);
-            img.removeEventListener('load', load);
-            img.removeEventListener('mouseup', removeDrag);
-            img.removeEventListener('mousemove', drag);
-            img.removeEventListener('mousedown', draggable);
-            img.removeEventListener('wheel', onwheel);
-
-            img.style.backgroundImage = originalProperties.backgroundImage;
-            img.style.backgroundRepeat = originalProperties.backgroundRepeat;
-            img.src = originalProperties.src;
-        }.bind(null, {
-            backgroundImage: img.style.backgroundImage,
-            backgroundRepeat: img.style.backgroundRepeat,
-            src: img.src
-        });
-
-        img.addEventListener('wheelzoom.destroy', destroy);
-
-        if (img.complete) {
-            load();
-        }
-
-        img.addEventListener('load', load);
-    };
-
-    // Do nothing in IE8
-    if (typeof window.getComputedStyle !== 'function') {
-        return function(elements) {
-            return elements;
-        };
-    } else {
-        return function(elements, settings) {
-            if (elements && elements.length) {
-                Array.prototype.forEach.call(elements, main, settings);
-            } else if (elements && elements.nodeName) {
-                main(elements, settings);
-            }
-            return elements;
-        };
-    }
-}());
-
-var resetButtonWheelZoom = document.createElement('button');
-
 window.wheelzoom2 = (function(){
 
     var canvas = document.createElement('canvas');
+    var canvas2 = document.createElement('canvas');
 
     var main = function(img, image2, settings){
         if (!img || !img.nodeName || img.nodeName !== 'IMG') { return; }
@@ -238,7 +43,7 @@ window.wheelzoom2 = (function(){
         var height;
         var previousEvent;
         var cachedDataUrl;
-        resetButtonWheelZoom.addEventListener('onclick', reset);            
+        var cachedDataUrl2;
 
         function setSrcToBackground(img) {
             img.style.backgroundImage = 'url("'+img.src+'")';
@@ -253,6 +58,20 @@ window.wheelzoom2 = (function(){
 
             reset();
         }
+
+        function setSrcToBackground2(img) {
+            img.style.backgroundImage = 'url("'+img.src+'")';
+            img.style.backgroundRepeat = 'no-repeat';
+            canvas2.width = img.naturalWidth;
+            canvas2.height = img.naturalHeight;
+            // canvas.height = Math.min(720, canvas.width * (img.imHeight / img.imWidth));
+            img.bgOffsetX = (canvas.width - img.naturalWidth)/2;
+            img.bgOffsetY = (canvas.height - img.naturalHeight)/2;
+            cachedDataUrl2 = canvas2.toDataURL();
+            img.src = cachedDataUrl2;
+
+            // reset2();
+        }        
 
         function updateBgStyle() {
 
@@ -305,7 +124,10 @@ window.wheelzoom2 = (function(){
         }
 
         function reset() {
-            console.log('reset!!')
+            canvas.width = img.naturalWidth;
+            canvas.height = img.naturalHeight;
+            img.bgOffsetX = (canvas.width - img.naturalWidth)/2;
+            img.bgOffsetY = (canvas.height - img.naturalHeight)/2;
 
             if (canvas) {
                 var zoomFactor = 2*Math.min(canvas.width / img.imWidth, canvas.height / img.imHeight);
@@ -315,17 +137,11 @@ window.wheelzoom2 = (function(){
 
             img.bgWidth = canvas.width;
             img.bgHeight = canvas.height;
-            // var initialZoomLevel = 1;
-            // img.bgWidth = img.naturalWidth * initialZoomLevel; // initialZoomLevel could be 1 for full size
-            // img.bgHeight = img.naturalHeight * initialZoomLevel;
             img.bgPosX = (img.imWidth - img.bgWidth) / 2;
             img.bgPosY = (img.imHeight - img.bgHeight) / 2;
 
             image2.bgWidth = canvas.width;
             image2.bgHeight = canvas.height;
-            // var initialZoomLevel = 1;
-            // image2.bgWidth = image2.naturalWidth * initialZoomLevel; // initialZoomLevel could be 1 for full size
-            // image2.bgHeight = image2.naturalHeight * initialZoomLevel;
             image2.bgPosX = (image2.imWidth - image2.bgWidth) / 2;
             image2.bgPosY = (image2.imHeight - image2.bgHeight) / 2;
 
@@ -425,7 +241,7 @@ window.wheelzoom2 = (function(){
             img.addEventListener('wheel', onwheel);
             img.addEventListener('mousedown', draggable);            
 
-            if (image2.src === cachedDataUrl) return;
+            if (image2.src === cachedDataUrl2) return;
 
             image2.imWidth = image2.naturalWidth;
             image2.imHeight = image2.naturalHeight;
@@ -438,7 +254,7 @@ window.wheelzoom2 = (function(){
             image2.style.backgroundSize     = image2.bgWidth+'px '+image2.bgHeight+'px';
             image2.style.backgroundPosition = image2.bgPosX+' '+image2.bgPosY;
 
-            setSrcToBackground(image2);
+            setSrcToBackground2(image2);
 
             image2.addEventListener('wheelzoom.reset', reset);
             image2.addEventListener('wheel', onwheel);
@@ -469,15 +285,13 @@ window.wheelzoom2 = (function(){
             image2.style.backgroundImage = originalProperties.backgroundImage2;
             image2.style.backgroundRepeat = originalProperties.backgroundRepeat2;
             image2.src = originalProperties.src2;
-
         }.bind(null, {
             backgroundImage: img.style.backgroundImage,
             backgroundRepeat: img.style.backgroundRepeat,
             src: img.src,
-            backgroundImage2: img.style.backgroundImage,
-            backgroundRepeat2: img.style.backgroundRepeat,
-            src2: img.src
-
+            backgroundImage2: image2.style.backgroundImage,
+            backgroundRepeat2: image2.style.backgroundRepeat,
+            src2: image2.src
         });
 
         img.addEventListener('wheelzoom.destroy', destroy);
@@ -488,13 +302,13 @@ window.wheelzoom2 = (function(){
 
         img.addEventListener('load', load);
 
-        image2.addEventListener('wheelzoom.destroy', destroy);
+        // image2.addEventListener('wheelzoom.destroy', destroy);
 
-        if (image2.complete) {
-            load();
-        }
+        // if (image2.complete) {
+        //     load();
+        // }
 
-        image2.addEventListener('load', load);
+        // image2.addEventListener('load', load);
     };
 
     // Do nothing in IE8
